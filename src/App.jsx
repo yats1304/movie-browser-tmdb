4509e0ac04b06dcbe3ad264401d022568ea2d3fc
yats1304 <chaubaly@gmail.com>
2025-08-06 10:48:46 +0530

    Fix: correct component import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Search from './components/search'
+import Search from './components/Search'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard';
 
